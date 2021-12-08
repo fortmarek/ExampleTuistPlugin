3fdbe2700fd6e2b611b08876cbe7116284555617
fortmarek <marekfort@me.com>
2021-12-08 08:51:37 +0100

    Update ProjectAutomation

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/tuist/tuist", .revision("8b5aa8a4c")),
+        .package(url: "https://github.com/tuist/tuist", .revision("37d054762")),
     ],
     targets: [
         .target(
